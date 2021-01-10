b169bc218499da98c851bf2ed586b16ad517755f
mitranagabriela <mitrana_gabriela2008@yahoo.com>
2021-01-10 17:51:33 +0100

    Fixed styling

--- src/pages/EventDetails.js
+++ src/pages/EventDetails.js
@@ -3,10 +3,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
   Button,
-  Box,
-  Image,
-  Stack,
-  Flex,
   Text,
   Heading,
   UnorderedList,
@@ -14,15 +10,7 @@ import {
 } from '@chakra-ui/react';
 import { LiveMessage } from 'react-aria-live';
 import { Link as RouterLink } from 'react-router-dom';
-//import EventOverview from '../components/EventOverview';
-
-const dateFromString = function (dateString) {
-  return new Intl.DateTimeFormat('en-GB', {
-    year: 'numeric',
-    month: 'long',
-    day: '2-digit',
-  }).format(new Date(dateString));
-};
+import { EventOverview } from '../components';
 
 class EventDetails extends Component {
   constructor(props) {
@@ -44,131 +32,52 @@ class EventDetails extends Component {
           clearOnUnmount="true"
         />
 
-        <Flex
-          align="center"
-          justify={{
-            base: 'center',
-            md: 'space-around',
-            xl: 'space-between',
-          }}
-          direction={{ base: 'column-reverse', md: 'row' }}
-          wrap="no-wrap"
-          minH="auto"
-          //px={8}
-          //mb={16}
+        <EventOverview eventdetails={this.props.details} />
+
+        <Heading
+          as="h2"
+          textStyle="h2"
+          lineHeight={2}
+          textAlign={['center', 'center', 'left', 'left']}
         >
-          <Stack
-            spacing={2}
-            w={{ base: '80%', md: '40%' }}
-            align={['center', 'center', 'flex-start', 'flex-start']}
-          >
-            <Heading
-              as="h1"
-              size="xl"
-              lineHeight={2}
-              textAlign={['center', 'center', 'left', 'left']}
-            >
-              {this.props.details['name']}
-            </Heading>
-            <Text
-              textStyle="paragraph"
-              lineHeight={2}
-              textAlign={['center', 'center', 'left', 'left']}
-            >
-              {'Date: ' + dateFromString(this.props.details.dateTime)}
-            </Text>
-            <Text
-              textStyle="paragraph"
-              lineHeight={2}
-              textAlign={['center', 'center', 'left', 'left']}
-            >
-              {'Location: ' + this.props.details.location}
-            </Text>
-            <Text
-              textStyle="paragraph"
-              lineHeight={2}
-              textAlign={['center', 'center', 'left', 'left']}
-            >
-              {'Telephone number: ' + this.props.details.phone}
-            </Text>
-            <Text
-              stextStyle="paragraph"
-              lineHeight={2}
-              textAlign={['center', 'center', 'left', 'left']}
-            >
-              {'Association: ' + this.props.details.association}
-            </Text>
-            <Button
-              as={RouterLink}
-              to={'/event/' + this.props.details['id'] + '/register'}
-              variant="outline"
-              colorScheme="black"
-            >
-              Apply now
-            </Button>
-          </Stack>
-          <Box
-            w={{ base: '80%', sm: '60%', md: '50%' }}
-            mb={{ base: 12, md: 0 }}
-          >
-            <Image
-              boxSize="300px"
-              objectFit="cover"
-              src={
-                process.env.PUBLIC_URL + '/' + this.props.details.pictures[0]
-              }
-              alt={this.props.details.altPictures[0]}
-              rounded="1rem"
-              shadow="2xl"
-            />
-          </Box>
-        </Flex>
-        <Flex direction="column" maxW={{ xl: '1200px' }} m="0 auto">
-          <Heading
-            as="h2"
-            textStyle="h2"
-            lineHeight={2}
-            textAlign={['center', 'center', 'left', 'left']}
-          >
-            {'Description of the event'}
-          </Heading>
-          <Text
-            textStyle="paragraph"
-            lineHeight={2}
-            textAlign={['center', 'center', 'left', 'left']}
-          >
-            {this.props.details.longDescription}
-          </Text>
-          <Heading
-            as="h2"
-            textStyle="h2"
-            lineHeight={2}
-            textAlign={['center', 'center', 'left', 'left']}
-          >
-            {'Activities'}
-          </Heading>
-          <Text
-            textStyle="paragraph"
-            lineHeight={2}
-            textAlign={['center', 'center', 'left', 'left']}
-          >
-            <UnorderedList>
-              {this.props.details.activities.map((listitem) => (
-                <ListItem>{listitem}</ListItem>
-              ))}
-            </UnorderedList>
-          </Text>
-          <br></br>
-          <br></br>
-          <Button
-            as={RouterLink}
-            to={'/event/' + this.props.details['id'] + '/register'}
-            variant="outline"
-            colorScheme="black"
-          >
-            Apply now
-          </Button>
-        </Flex>
+          {'Description of the event'}
+        </Heading>
+        <Text
+          textStyle="paragraph"
+          lineHeight={2}
+          textAlign={['center', 'center', 'left', 'left']}
+        >
+          {this.props.details.longDescription}
+        </Text>
+        <Heading
+          as="h2"
+          textStyle="h2"
+          lineHeight={2}
+          textAlign={['center', 'center', 'left', 'left']}
+        >
+          {'Activities'}
+        </Heading>
+        <Text
+          textStyle="paragraph"
+          lineHeight={2}
+          textAlign={['center', 'center', 'left', 'left']}
+        >
+          <UnorderedList>
+            {this.props.details.activities.map((listitem) => (
+              <ListItem>{listitem}</ListItem>
+            ))}
+          </UnorderedList>
+        </Text>
+        <br></br>
+        <br></br>
+        <Button
+          as={RouterLink}
+          to={'/event/' + this.props.details['id'] + '/register'}
+          variant="outline"
+          colorScheme="brand"
+        >
+          Apply now
+        </Button>
       </React.Fragment>
     );
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Flex, Heading, Text, ListItem, OrderedList } from '@chakra-ui/react';
+import { Heading, Text, ListItem, OrderedList } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 import { LiveMessage } from 'react-aria-live';
 import { Hero } from '../components';
@@ -23,76 +23,74 @@ class Home extends Component {
           clearOnUnmount="true"
         />
         <Hero />
-        <Flex direction="column" maxW={{ xl: '1200px' }} m="0 auto">
-          <Heading
-            as="h2"
-            textStyle="h1"
-            lineHeight={2}
-            textAlign={['center', 'center', 'left', 'left']}
-          >
-            {'Our mission'}
-          </Heading>
-          <Text
-            textStyle="paragraph"
-            lineHeight={2}
-            textAlign={['center', 'center', 'left', 'left']}
-            fontSize="lg"
-          >
-            {'Our mission'}
-          </Text>
-          <Heading
-            as="h2"
-            textStyle="h1"
-            lineHeight={2}
-            textAlign={['center', 'center', 'left', 'left']}
-          >
-            {'How to apply to an event'}
-          </Heading>
-          <Text
-            textStyle="paragraph"
-            lineHeight={2}
-            textAlign={['center', 'center', 'left', 'left']}
-            fontSize="lg"
-          >
-            {
-              'The application process is simple and quick. You just need to follow four steps:'
-            }
-          </Text>
-          <Text
-            textStyle="paragraph"
-            lineHeight={2.5}
-            textAlign={['center', 'center', 'left', 'left']}
-            fontSize="lg"
-          >
-            <OrderedList>
-              <ListItem>Browse our wide selection of events.</ListItem>
-              <ListItem>
-                Filter events by topic and location to find your best match.
-              </ListItem>
-              <ListItem>
-                Apply to your chosen event and fill in the form.
-              </ListItem>
-              <ListItem>Now you only have to attend and enjoy!</ListItem>
-            </OrderedList>
-          </Text>
-          <Heading
-            as="h2"
-            textStyle="h1"
-            lineHeight={2}
-            textAlign={['center', 'center', 'left', 'left']}
-          >
-            {'Recommended events'}
-          </Heading>
-          <Heading
-            as="h2"
-            textStyle="h1"
-            lineHeight={2}
-            textAlign={['center', 'center', 'left', 'left']}
-          >
-            {'Our benefits'}
-          </Heading>
-          <Grid />
-        </Flex>
+        <Heading
+          as="h2"
+          textStyle="h1"
+          lineHeight={2}
+          textAlign={['center', 'center', 'left', 'left']}
+        >
+          {'Our mission'}
+        </Heading>
+        <Text
+          textStyle="paragraph"
+          lineHeight={2}
+          textAlign={['center', 'center', 'left', 'left']}
+          fontSize="lg"
+        >
+          {'Our mission'}
+        </Text>
+        <Heading
+          as="h2"
+          textStyle="h1"
+          lineHeight={2}
+          textAlign={['center', 'center', 'left', 'left']}
+        >
+          {'How to apply to an event'}
+        </Heading>
+        <Text
+          textStyle="paragraph"
+          lineHeight={2}
+          textAlign={['center', 'center', 'left', 'left']}
+          fontSize="lg"
+        >
+          {
+            'The application process is simple and quick. You just need to follow four steps:'
+          }
+        </Text>
+        <Text
+          textStyle="paragraph"
+          lineHeight={2.5}
+          textAlign={['center', 'center', 'left', 'left']}
+          fontSize="lg"
+        >
+          <OrderedList>
+            <ListItem>Browse our wide selection of events.</ListItem>
+            <ListItem>
+              Filter events by topic and location to find your best match.
+            </ListItem>
+            <ListItem>
+              Apply to your chosen event and fill in the form.
+            </ListItem>
+            <ListItem>Now you only have to attend and enjoy!</ListItem>
+          </OrderedList>
+        </Text>
+        <Heading
+          as="h2"
+          textStyle="h1"
+          lineHeight={2}
+          textAlign={['center', 'center', 'left', 'left']}
+        >
+          {'Recommended events'}
+        </Heading>
+        <Heading
+          as="h2"
+          textStyle="h1"
+          lineHeight={2}
+          textAlign={['center', 'center', 'left', 'left']}
+        >
+          {'Our benefits'}
+        </Heading>
+        <Grid />
       </>
     );
   }
