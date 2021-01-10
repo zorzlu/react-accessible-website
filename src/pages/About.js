a6d9faff6437bc486a7dd6446ff3660adc997cbe
mitranagabriela <mitrana_gabriela2008@yahoo.com>
2021-01-10 19:28:07 +0100

    About us page v1

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Heading } from '@chakra-ui/react';
+import { Heading, UnorderedList, ListItem, Text } from '@chakra-ui/react';
 import { LiveMessage } from 'react-aria-live';
+import { Grid } from '../components';
 
 class About extends Component {
   constructor(props) {
@@ -21,10 +22,75 @@ class About extends Component {
           aria-live="polite"
           clearOnUnmount="true"
         />
-        <Heading as="h1" size="xl">
-          About Us
+        <Heading
+          as="h1"
+          textStyle="h1"
+          lineHeight={2}
+          textAlign={['center', 'center', 'left', 'left']}
+        >
+          {'Who are we'}
         </Heading>
-        <div className="About">This is the About page.</div>
+        <Text
+          textStyle="paragraph"
+          lineHeight={2}
+          textAlign={['center', 'center', 'left', 'left']}
+          fontSize="lg"
+        >
+          {'Description about us'}
+        </Text>
+        <Heading
+          as="h2"
+          textStyle="h1"
+          lineHeight={2}
+          textAlign={['center', 'center', 'left', 'left']}
+        >
+          {'History'}
+        </Heading>
+        <Text
+          textStyle="paragraph"
+          lineHeight={2}
+          textAlign={['center', 'center', 'left', 'left']}
+          fontSize="lg"
+        >
+          {'Some more infor about our path.'}
+        </Text>
+        <Heading
+          as="h2"
+          textStyle="h1"
+          lineHeight={2}
+          textAlign={['center', 'center', 'left', 'left']}
+        >
+          {'Our values'}
+        </Heading>
+        <Text
+          textStyle="paragraph"
+          lineHeight={2.5}
+          textAlign={['center', 'center', 'left', 'left']}
+          fontSize="lg"
+        >
+          <UnorderedList>
+            <ListItem>Integrity</ListItem>
+            <ListItem>Flexibility</ListItem>
+            <ListItem>Respect</ListItem>
+          </UnorderedList>
+        </Text>
+        <Heading
+          as="h2"
+          textStyle="h1"
+          lineHeight={2}
+          textAlign={['center', 'center', 'left', 'left']}
+        >
+          {'Our figures'}
+        </Heading>
+        <Grid />
+        <Heading
+          as="h2"
+          textStyle="h1"
+          lineHeight={2}
+          textAlign={['center', 'center', 'left', 'left']}
+        >
+          {'Meet the team'}
+        </Heading>
       </>
     );
   }
