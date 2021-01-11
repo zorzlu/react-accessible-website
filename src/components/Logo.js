560ba0a2bd36ae1292fa3ac7f395ecbbeabcd4d2
Luca <37113780+zorzlu@users.noreply.github.com>
2021-01-11 02:20:36 +0100

    no motion in logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import {
-  Box,
-  Image,
-  keyframes,
-  usePrefersReducedMotion,
-} from '@chakra-ui/react';
+import { Box, Image } from '@chakra-ui/react';
 import { NavLink } from 'react-router-dom';
 
-const spin = keyframes`
-  from { transform: rotate(0deg); }
-  to { transform: rotate(360deg); }
-`;
-
 const Logo = (props) => {
-  const prefersReducedMotion = usePrefersReducedMotion();
-
-  const animation = prefersReducedMotion
-    ? undefined
-    : `${spin} infinite 20s linear`;
-
   return (
     <Box
       w="2.5em"
@@ -31,7 +15,6 @@ const Logo = (props) => {
       <NavLink to="/">
         <Image
           boxSize="2.5em"
-          animation={animation}
           src={process.env.PUBLIC_URL + 'logo.svg'}
           alt="Application Logo, back to Homepage."
         />
