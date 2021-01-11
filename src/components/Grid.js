7ad304f09e3f9aee8ceffa3532accb9e55aff276
mitranagabriela <mitrana_gabriela2008@yahoo.com>
2021-01-11 22:59:26 +0100

    Changed About us page

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -11,25 +11,27 @@ import {
 
 const Grid = () => {
   return (
-    <Table variant="simple" role="table" aria-label="Replace">
-      <TableCaption>Caption</TableCaption>
+    <Table
+      variant="simple"
+      role="table"
+      aria-label="CommunityMate figures"
+      allign="center"
+    >
+      <TableCaption>CommunityMate numbers</TableCaption>
       <Thead role="rowgroup">
         <Tr role="row">
-          <Th role="columnheader">Columnheader1</Th>
-          <Th role="columnheader">Columnheader2</Th>
-          <Th role="columnheader">Columnheader3</Th>
+          <Th role="columnheader">Partners</Th>
+          <Th role="columnheader">Events</Th>
         </Tr>
       </Thead>
       <Tbody role="rowgroup">
         <Tr role="row">
-          <Td role="cell">Data1</Td>
-          <Td role="cell">Data2</Td>
-          <Td role="cell">Data3</Td>
+          <Td role="cell">15+ Associations</Td>
+          <Td role="cell">30+ Past events</Td>
         </Tr>
         <Tr role="row">
-          <Td role="cell">Data4</Td>
-          <Td role="cell">Data5</Td>
-          <Td role="cell">Data6</Td>
+          <Td role="cell">50+ Volunteers</Td>
+          <Td role="cell">45+ Upcoming events</Td>
         </Tr>
       </Tbody>
     </Table>
